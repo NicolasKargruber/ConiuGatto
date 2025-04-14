8dfc6e164899b35eaa9a1afd93bc674316f77feb
Nicolas Kargruber <nicolas.kargruber@gmail.com>
2025-04-14 20:33:39 +0200

    [CON-40] Show current solution

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -55,89 +55,92 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings ⚙️")),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: double.maxFinite,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              color: Colors.black12,
-              child: Text(
-                "Tenses",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                color: Colors.black12,
+                child: Text(
+                  "Tenses",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 8),
-          ListTile(
-            title: Text("The following tenses will be included in your Quiz."),
-          ),
-          ListTile(
-            title: Center(
-              child: FilledButton.tonal(
-                onPressed: () => _showChooseTensesSheet(context),
-                child: Text("Update tenses"),
+            SizedBox(height: 8),
+            ListTile(
+              title: Text("The following tenses will be included in your Quiz."),
+            ),
+            ListTile(
+              title: Center(
+                child: FilledButton.tonal(
+                  onPressed: () => _showChooseTensesSheet(context),
+                  child: Text("Update tenses"),
+                ),
               ),
             ),
-          ),
-
-          SizedBox(height: 8),
-
-          SizedBox(
-            width: double.maxFinite,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              color: Colors.black12,
-              child: Text(
-                "Verbs",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+        
+            SizedBox(height: 8),
+        
+            SizedBox(
+              width: double.maxFinite,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                color: Colors.black12,
+                child: Text(
+                  "Verbs",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 8),
-          ListTile(
-            title: Text(
-              "The following verbs will be included up in your Quiz.",
-            ),
-          ),
-          ListTile(
-            title: Center(
-              child: FilledButton.tonal(
-                onPressed: () => _showChooseVerbsSheet(context),
-                child: Text("Update verbs"),
+            SizedBox(height: 8),
+            ListTile(
+              title: Text(
+                "The following verbs will be included up in your Quiz.",
               ),
             ),
-          ),
-
-          SizedBox(height: 8),
-
-          SizedBox(
-            width: double.maxFinite,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              color: Colors.black12,
-              child: Text(
-                "Pronouns",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+            ListTile(
+              title: Center(
+                child: FilledButton.tonal(
+                  onPressed: () => _showChooseVerbsSheet(context),
+                  child: Text("Update verbs"),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 8),
-          ListTile(
-            title: Text(
-              "The following pronouns will be included up in your Quiz.",
-            ),
-          ),
-          ListTile(
-            title: Center(
-              child: FilledButton.tonal(
-                onPressed: ()=> _showChoosePronounsSheet(context),
-                child: Text("Update pronouns"),
+        
+            SizedBox(height: 8),
+        
+            SizedBox(
+              width: double.maxFinite,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                color: Colors.black12,
+                child: Text(
+                  "Pronouns",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 8),
+            ListTile(
+              title: Text(
+                "The following pronouns will be included up in your Quiz.",
+              ),
+            ),
+            ListTile(
+              title: Center(
+                child: FilledButton.tonal(
+                  onPressed: ()=> _showChoosePronounsSheet(context),
+                  child: Text("Update pronouns"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
